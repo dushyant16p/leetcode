474ba89f87e5785ed0e6b7661b5da6ec3a6043e3
dushyant <dushyantprajapat70@gmail.com>
2026-01-15 11:17:48 +0530

    Time: 0 ms (100.00%) | Memory: 24.3 MB (7.53%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        int j=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-             swap(nums[j],nums[i]);
-                j++;
-            }
-        }
-        
+        stable_partition(nums.begin(),nums.end(),[](int x){
+        return x!=0;
+    });
     }
 };
