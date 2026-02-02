dd50c910e6df0cc84b003d93324053a9cdaec688
dushyant <dushyantprajapat70@gmail.com>
2026-02-02 11:25:26 +0530

    Time: 4 ms (35.43%) | Memory: 9.7 MB (48.19%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.size()!=t.size()) return false;
+
+        map<char,int>m;
+        for(auto x:s){
+            m[x]++;
+        }
+        for(auto x:t){
+            m[x]--;
+        }
+        for(auto x:m){
+            if(x.second!=0)return false;
+        }
+
+        return true;
+
+    }
+};
